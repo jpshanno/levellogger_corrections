11b093bc1b38629c4d648be2caf0bfe9bc9c084e
Joe Shannon <jpshanno@mtu.edu>
2020-08-11 08:28:24 -0400

    Updated to allow mclapply to run properly. Sets drake to run in a new environment rather than the global.

--- _drake.R
+++ _drake.R
@@ -1,8 +1,12 @@
-source("code/levellogger_packages.R")
-source("code/levellogger_functions.R")
-source("code/levellogger_plan.R")
+# https://books.ropensci.org/drake/hpc.html#parallel-computing-within-targets
+envir <- new.env(parent = globalenv())
+
+source("code/levellogger_packages.R", local = envir)
+source("code/levellogger_functions.R", local = envir)
+source("code/levellogger_plan.R", local = envir)
 
 drake_config(
-  plan, # defined in R/plan.R
-  verbose = 2
+  envir$plan, # defined in R/plan.R
+  verbose = 2,
+  envir = envir
 )
--- make.R
+++ make.R
@@ -1,9 +1,13 @@
-source("code/levellogger_packages.R")
-source("code/levellogger_functions.R")
-source("code/levellogger_plan.R")
+# https://books.ropensci.org/drake/hpc.html#parallel-computing-within-targets
+envir <- new.env(parent = globalenv())
+
+source("code/levellogger_packages.R", local = envir)
+source("code/levellogger_functions.R", local = envir)
+source("code/levellogger_plan.R", local = envir)
 
 # Run the plan from code/levellogger_plan.R
 make(
-  plan,
-  verbose = 2
+  envir$plan,
+  verbose = 2,
+  envir = envir
 )
