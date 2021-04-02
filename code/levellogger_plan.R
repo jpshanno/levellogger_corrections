615efb8f48cd44734aa58207fc54ed356dff1538
Joe Shannon <jpshanno@mtu.edu>
2021-04-01 23:09:11 -0400

    Removed subset of water_balance calucation and readded water balance adjust when et < 0

--- code/levellogger_plan.R
+++ code/levellogger_plan.R
@@ -63,13 +63,11 @@ plan <-
 
     # Calculate water balance components
     water_balance =
-      # subset_case_study(data = case_study,
-      #                   start = "2018-08-09 00:00",
-      #                   end = "2018-08-23 06:00") %>%
       case_study %>% 
       calculate_detrended_g() %>%
       calculate_delta_s() %>%
       calculate_et() %>% 
+      adjust_water_balance() %>% 
       drop_trailing_data("2018-08-23"),
 
 # Figures -----------------------------------------------------------------
